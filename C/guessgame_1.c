be4186ca8b776e8560917d41684f5b5934666432
Dragonixj <delafiado@gmail.com>
2023-04-08 21:51:17 +0000

    commit made

---
+++ C/guessgame_1.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+  srand(time(0));                // seed random number generator
+  int number = rand() % 100 + 1; // generate random number between 1 and 100
+  int guess;
+  int attempts = 0;
+
+  printf("Welcome to the guessing game!\n Can you guess the number I'm "
+         "thinking of between 1 and 100?\n");
+
+  do {
+    printf("Enter your guess: ");
+    scanf("%d", &guess);
+    attempts++;
+
+    if (guess < number) {
+      printf("Too low! Try again.\n");
+    } else if (guess > number) {
+      printf("Too high! Try again.\n");
+    } else {
+      printf("Congratulations! You guessed the number in %d attempts.\n",
+             attempts);
+      break;
+    }
+
+    if (attempts == 10) {
+      printf("Sorry, you've used up all your attempts. The number was %d.\n",
+             number);
+      break;
+    }
+
+    if (attempts % 3 == 0) {
+      printf("Here's a hint: The number is divisible by %d.\n", attempts);
+    }
+
+  } while (guess != number);
+
+  return 0;
+}
