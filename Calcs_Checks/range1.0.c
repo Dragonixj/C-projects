eca6ca5c1ad8411e1e8ae8c8ece3410148d74b79
Dragonixj <delafiado@gmail.com>
2023-04-17 06:22:59 +0000

    commit made

---
+++ Calcs_Checks/range1.0.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+  int input;
+  char choice;
+
+  do {
+    printf("Enter a number between 1 and 500\n");
+    printf("\n");
+    printf("->>");
+    scanf("%d", &input);
+
+    if (input >= 1 && input <= 100) {
+      printf("Your number is between 1 and 100.\n");
+    } else if (input >= 101 && input <= 200) {
+      printf("Your number is between 101 and 200.\n");
+    } else if (input >= 201 && input <= 300) {
+      printf("Your number is between 201 and 300.\n");
+    } else if (input >= 301 && input <= 400) {
+      printf("Your number is between 301 and 400.\n");
+    } else if (input >= 401 && input <= 500) {
+      printf("Your number is between 401 and 500.\n");
+    } else {
+      printf("We have no such range of values.\n");
+    }
+
+    printf("Do you want to continue (Y/N)?\n");
+    printf("\n");
+    printf("->>");
+    scanf(" %c", &choice);
+  } while (choice == 'Y' || choice == 'y');
+
+  return 0;
+}
